2797e0db93b3e376dd3a5c9d8ee523680d8a46e3
Soumya B <Soumya1.B@ril.com>
2023-01-29 00:48:32 +0530

    Add index to route

---
+++ src/routes/v1/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const authRoute = require('./auth.route');
+const userRoute = require('./user.route');
+const postRoute = require('./post.route');
+const todoRoute = require('./todo.route');
+const config = require('../../config/config');
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/users',
+    route: userRoute,
+  },
+  {
+    path: '/posts',
+    route: postRoute,
+  },
+  {
+    path: '/todos',
+    route: todoRoute,
+  },
+];
+
+const devRoutes = [
+  // routes available only in development mode
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
+
+module.exports = router;
